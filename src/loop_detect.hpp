a797a00b72e7df813336687adfaf404ee3ffc463
Yuta Kohno <y.kohno1215@gmial.com>
2021-08-10 00:18:32 +0900

    refactor

--- src/loop_detect.hpp
+++ src/loop_detect.hpp
@@ -2,35 +2,49 @@
 
 using namespace std;
 
-vector<int> G_in[100000];
-vector<int> G_out[100000];
-int cnt_in[100000];
-int cnt_out[100000];
-bool used[100000];
+class LoopDetect{
+    public:
+    int n;
+    vector<vector<int>> g;
+    vector<vector<int>> g_in;
+    vector<bool> used;
+    vector<int> cnt_in;
+    vector<int> cnt_out;
+    LoopDetect(vector<vector<int>> g): g(g){
+        n = g.size();
+        used.resize(n);
+        cnt_in.resize(n);
+        cnt_out.resize(n);
+        g_in.resize(n);
+        for(int i = 0; i < n; i++){
+            for(int to: g[i]) g_in[to].push_back(i);
+        }
+        for(int i = 0; i < n; i++) used[i] = true;
+        for(int i = 0; i < n; i++) cnt_in[i] = g_in[i].size();
+        for(int i = 0; i < n; i++) cnt_out[i] = g[i].size();
+    };
 
-void dfs(int v){
-    used[v] = false;
-    for(int from : G_in[v]){
-        cnt_out[from]--;
-        if(cnt_out[from] == 0 && used[from]) dfs(from);
-    }
-    for(int to : G_out[v]){
-        cnt_in[to]--;
-        if(cnt_in[to] == 0 && used[to]) dfs(to);        
-    }
-}
-
-bool loop_detect(int n_vertex){
-    for(int i = 0; i < n_vertex; i++) used[i] = true;
-    for(int i = 0; i < n_vertex; i++) cnt_in[i] = G_in[i].size();
-    for(int i = 0; i < n_vertex; i++) cnt_out[i] = G_out[i].size();
-    for(int i = 0; i < n_vertex; i++){
-        if(cnt_in[i] == 0 || cnt_out[i] == 0){
-            if(used[i]) dfs(i);
+    void dfs(int v){
+        used[v] = false;
+        for(int from : g_in[v]){
+            cnt_out[from]--;
+            if(cnt_out[from] == 0 && used[from]) dfs(from);
+        }
+        for(int to : g[v]){
+            cnt_in[to]--;
+            if(cnt_in[to] == 0 && used[to]) dfs(to);        
         }
     }
-    for(int i = 0; i < n_vertex; i++) {
-        if(used[i]) return true;
+
+    bool loop_detect(){
+        for(int i = 0; i < n; i++){
+            if(cnt_in[i] == 0 || cnt_out[i] == 0){
+                if(used[i]) dfs(i);
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            if(used[i]) return true;
+        }
+        return false;
     }
-    return false;
-}
+};
--- src/utils.hpp
+++ src/utils.hpp
@@ -34,3 +34,35 @@ void print_vector(vector<T> v, char delimiter=' '){
     for(int i = 0; i+1 < v.size(); i++) cout << v[i] << delimiter;
     cout << v.back() << endl;
 }
+
+template<typename T>
+vector<T> naive_convolution(vector<T> u, vector<T> v){
+    int n = u.size(), m = v.size();
+    vector<T> ans(n+m-1);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            ans[i+j] += u[i]*v[j];
+        }
+    }
+    return ans;
+}
+
+/**
+ * f(x) = g(x)*h(x) + r(x)となる(h(x), r(x))を求めます
+ */ 
+template<typename T>
+pair<vector<T>, vector<T>> naive_divide(vector<T> f, vector<T> g){
+    int n = f.size()-1;
+    int m = g.size()-1;
+    if(n < m) return make_pair(vector<T>(1, T(0)), f);
+    int k = n-m;
+    vector<T> h(k+1);
+    T iv = T(1)/g[m];
+    for(int i = k; i >= 0; i--){
+        h[i] = f[i+m]*iv;
+        for(int j = 0; j <= m; j++){
+            f[i+j] -= g[j]*h[i];
+        }
+    }
+    return make_pair(h, f);
+}
