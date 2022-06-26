e5b3559e4af01d3cdb93ef908bd585ab631a164f
Yuta Kohno <y.kohno1215@gmial.com>
2022-06-26 19:48:44 +0900

    add mo

--- src/arg_sort.hpp
+++ src/arg_sort.hpp
@@ -16,15 +16,15 @@ ostream& operator<<(ostream& os, const point<T>& p){
 
 /**
  * for sorting by atan2(x, y)
- * verified: https://old.yosupo.jp/submission/36595
+ * verified: https://judge.yosupo.jp/submission/91704
  */
 template<typename T>
 bool comp(point<T> p1, point<T> p2){
     if(p1.y >= 0 && p2.y < 0) return false;
     if(p1.y < 0 && p2.y >= 0) return true;
-    if(p1.y == 0 && p2.y == 0) return p1.x >= p2.x;
-    if(p1.y == 0 && p1.x == 0) return p2.y >= 0;
-    if(p2.y == 0 && p2.x == 0) return p1.y <= 0;
+    if(p1.y == 0 && p2.y == 0) return p1.x > p2.x;
+    if(p1.y == 0 && p1.x == 0) return p2.y > 0;
+    if(p2.y == 0 && p2.x == 0) return p1.y < 0;
     return p1.y*p2.x < p1.x*p2.y;
 }
 
---
+++ src/mo.hpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+using namespace std;
+
+/**
+ * verified: https://codeforces.com/contest/1195/submission/161851257
+ */ 
+template<typename T>
+class Mo{
+    public:
+    vector<int> l, r;
+    int bucket_size;
+    Mo(int bucket_size): l(vector<int>()), r(vector<int>()), bucket_size(bucket_size) {}
+    void add_query(int l_, int r_){
+        l.push_back(l_);
+        r.push_back(r_);
+    }
+    template<typename ADD, typename DEL, typename GET_ANS>
+    vector<T> solve(ADD add, DEL del, GET_ANS get_ans){
+        int sz = l.size();
+        vector<int> ord(sz);
+        iota(ord.begin(), ord.end(), 0);
+        sort(ord.begin(), ord.end(), [&](const int a, const int b){
+            const int c = l[a] / bucket_size, d = l[b] / bucket_size;
+            return (c == d) ? ((c & 1) ? (r[b] < r[a]) : (r[a] < r[b])) : (c < d);
+        });
+        int li = 0, ri = 0;
+        vector<T> ans(sz);
+        for(const int i : ord){
+            while(li > l[i]) add(--li);
+            while(ri < r[i]) add(ri++);
+            while(li < l[i]) del(li++);
+            while(ri > r[i]) del(--ri);
+            ans[i] = get_ans();
+        }
+        return ans;
+    }
+};
