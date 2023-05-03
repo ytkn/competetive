99b0641ef84594ee006b000e6b6b814eeb14fb9b
Yuta Kohno <y.kohno1215@gmial.com>
2023-05-03 12:50:43 +0900

    add fps

---
+++ src/fps.hpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <atcoder/modint>
+#include <atcoder/convolution>
+
+using namespace std;
+
+using mint = atcoder::modint998244353;
+
+struct FPS: vector<mint>{
+    using vector<mint>::vector;
+
+    /**
+     * veryfied: https://judge.yosupo.jp/submission/137319 
+     */
+    FPS inv(int deg){
+        FPS ans(deg);
+        ans[0] = mint(1)/(*this)[0];
+        for(int d = 1; d < deg; d<<=1){
+            vector<mint> f(2*d), g(2*d);
+            for(int i = 0; i < min(2*d, (int)this->size()); i++) f[i] = (*this)[i];
+            for(int i = 0; i < d; i++) g[i] = ans[i];
+            f = atcoder::convolution(g, atcoder::convolution(g, f));
+            for(int i = d; i < min(2*d, deg); i++) ans[i] = -f[i];
+        }
+        return ans;
+    }
+
+    void shrink() {
+        while (this->size() && this->back() == mint(0)) this->pop_back();
+    }
+
+    FPS& operator*=(const FPS &r){
+        if(this->empty() || r.empty()){
+            this->clear();
+            return *this;
+        }
+        auto ret = atcoder::convolution(*this, r);
+        return *this = FPS(ret.begin(), ret.end());
+    }
+
+    FPS operator*(const FPS &r) const { 
+        return FPS(*this) *= r; 
+    }
+
+    FPS mod_deg(int deg){
+        FPS ret(deg);
+        for(int i = 0; i < min(deg, (int)this->size()); i++) ret[i] = (*this)[i];
+        return ret;
+    }
+};
