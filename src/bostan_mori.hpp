2bd3a52adb65d66155a9a4ca55f2a958cbab4d13
Yuta Kohno <y.kohno1215@gmial.com>
2023-05-07 00:49:07 +0900

    improve bostan-mori

--- src/bostan_mori.hpp
+++ src/bostan_mori.hpp
@@ -7,7 +7,8 @@ using namespace std;
 using mint = atcoder::modint998244353;
 
 /**
- * NOTE: qの末尾が0であってはならない
+ * NOTE: qの末尾が0であってはならない.
+ * for NTT-friendly modint
  */
 mint bostan_mori(vector<mint> p, vector<mint> q, long long n){
     assert(p.back().val() != 0);
@@ -38,6 +39,7 @@ mint bostan_mori(vector<mint> p, vector<mint> q, long long n){
 
 /**
  * s_n = s_{n-1}*c_0 + s_{n-2}*c_1 + ... + s_{n-d}*c_{d-1}
+ * for NTT-friendly modint
  */
 mint kth_element(vector<mint> c, vector<mint> s, long long k){
     int d = c.size();
@@ -49,4 +51,51 @@ mint kth_element(vector<mint> c, vector<mint> s, long long k){
     vector<mint> p(d);
     for(int i = 0; i < d; i++) p[i] = pp[i];
     return bostan_mori(p, q, k);
+}
+
+/**
+ * NOTE: qの末尾が0であってはならない
+ */
+template <class T, vector<T> (*multiply)(const vector<T>&, const vector<T>&)>
+T bostan_mori(vector<T> p, vector<T> q, long long n){
+    assert(p.back() != T(0));
+    int d = q.size()-1;
+    assert(p.size() <= d);
+    assert(q[0] == T(1));
+    if(n == 0){
+        return p[0];
+    }
+    vector<T> q_neg = q;
+    for(int i = 1; i < (int)q.size(); i+=2) q_neg[i] *= -1;
+    vector<T> pq = multiply(p, q_neg);
+    vector<T> qq = multiply(q, q_neg);
+    vector<T> v((qq.size()+1)/2);
+    for(int i = 0; i < qq.size(); i+=2){
+        v[i/2] = qq[i];
+    }
+    vector<T> u(n%2 == 0 ? (pq.size()+1)/2 : pq.size()/2);
+    for(int i = 0; i < pq.size(); i++){
+        if(n%2 == 0 && i%2 == 0){
+            u[i/2] = pq[i];
+        }else if(n%2 == 1 && i%2 == 1){
+            u[i/2] = pq[i];
+        }
+    }
+    return bostan_mori<T, multiply>(u, v, n/2);
+}
+
+/**
+ * s_n = s_{n-1}*c_0 + s_{n-2}*c_1 + ... + s_{n-d}*c_{d-1}
+ */
+template <class T, vector<T> (*multiply)(const vector<T>&, const vector<T>&)>
+T kth_element(vector<T> c, vector<T> s, long long k){
+    int d = c.size();
+    assert(s.size() == d);
+    vector<T> q(d+1);
+    q[0] = T(1);
+    for(int i = 0; i < d; i++) q[i+1] = -c[i];
+    auto pp = multiply(s, q);
+    vector<T> p(d);
+    for(int i = 0; i < d; i++) p[i] = pp[i];
+    return bostan_mori<T, multiply>(p, q, k);
 }
--- test/bostan_mori_test.cpp
+++ test/bostan_mori_test.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void test_bostan_mori(vector<mint> c, vector<mint> s, int k){
+void test_bostan_mori998244353(vector<mint> c, vector<mint> s, int k){
     int d = c.size();
     vector<mint> ss(k+1);
     for(int i = 0; i < d; i++) ss[i] = s[i];
@@ -17,7 +17,36 @@ void test_bostan_mori(vector<mint> c, vector<mint> s, int k){
     for(int i = 0; i <= k; i++) ASSERT_EQ(ss[i].val(), kth_element(c, s, i).val());
 }
 
-TEST(bostan_mori, bostan_mori) {
+template<typename T>
+vector<T> naive_convolution(const vector<T> &u, const vector<T> &v){
+    int n = u.size(), m = v.size();
+    vector<T> ans(n+m-1);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            ans[i+j] += u[i]*v[j];
+        }
+    }
+    return ans;
+}
+
+
+void test_bostan_mori1000000007(vector<atcoder::modint1000000007> c, vector<atcoder::modint1000000007> s, int k){
+    int d = c.size();
+    vector<atcoder::modint1000000007> ss(k+1);
+    for(int i = 0; i < d; i++) ss[i] = s[i];
+    for(int i = d; i <= k; i++){
+        for(int j = 0; j < d; j++){
+            ss[i] += c[j]*ss[i-j-1];
+        }
+    }
+    for(int i = 0; i <= k; i++) {
+        auto x = kth_element<atcoder::modint1000000007, naive_convolution>(c, s, i);
+        ASSERT_EQ(ss[i], x);
+    }
+}
+
+
+TEST(bostan_mori, test_bostan_mori998244353) {
     int n_iter = 10;
     int n = 50;
     vector<mint> c(n), s(n);
@@ -25,5 +54,16 @@ TEST(bostan_mori, bostan_mori) {
         c[i] = randint(1, 100);
         s[i] = randint(1, 100);
     }
-    test_bostan_mori(c, s, 1000);
+    test_bostan_mori998244353(c, s, 1000);
+}
+
+TEST(bostan_mori, test_bostan_mori1000000007) {
+    int n_iter = 10;
+    int n = 50;
+    vector<atcoder::modint1000000007> c(n), s(n);
+    for(int i = 0; i < n; i++){
+        c[i] = randint(1, 100);
+        s[i] = randint(1, 100);
+    }
+    test_bostan_mori1000000007(c, s, 1000);
 }
