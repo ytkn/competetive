aae0e28066a963a4097776e553de151cd2edda04
Yuta Kohno <y.kohno1215@gmial.com>
2023-10-09 23:14:02 +0900

    add exp, log

--- generator/generator_utils.cpp
+++ generator/generator_utils.cpp
@@ -2,7 +2,12 @@
 
 std::random_device rnd;
 std::mt19937 mt(rnd());
+std::mt19937_64 mt_ll(rnd());
 
 int randint(const int l, const int r){
     return mt()%(r - l) + l;
+}
+
+long long randll(const long long l, const long long r){
+    return mt_ll()%(r - l) + l;
 }
--- src/fps.hpp
+++ src/fps.hpp
@@ -6,8 +6,12 @@ using namespace std;
 
 using mint = atcoder::modint998244353;
 
+
 struct FPS: vector<mint>{
     using vector<mint>::vector;
+    /**
+     * verofied: https://judge.yosupo.jp/submission/137319
+     */
     FPS inv(int deg){
         FPS ans(deg);
         ans[0] = mint(1)/(*this)[0];
@@ -19,6 +23,46 @@ struct FPS: vector<mint>{
             for(int i = d; i < min(2*d, deg); i++) ans[i] = -f[i];
         }
         return ans;
+    }
+
+    FPS dev(){
+        FPS ans(this->size()-1);
+        for(int i = 1; i < this->size(); i++){
+            ans[i-1] = (*this)[i]*i;
+        }
+        return ans;
+    }
+
+    /**
+     * verified: https://judge.yosupo.jp/submission/165269
+     */
+    FPS log(int deg){
+        assert((*this)[0].val() == 1);
+        FPS f_inv = inv(deg);
+        FPS prod = (this->dev())*f_inv;
+        FPS ans(deg);
+        for(int i = 0; i < deg-1 && i < prod.size(); i++){
+            ans[i+1] = prod[i]/(i+1);
+        }
+        return ans;
+    }
+
+    /**
+     * verified: https://judge.yosupo.jp/submission/165270
+     */
+    FPS exp(int deg){
+        assert((*this)[0].val() == 0);
+        FPS f(1, mint(0));
+        FPS g(1, mint(1));
+        int d = 1;
+        while(f.size() < deg){
+            d <<= 1;
+            while(f.size() < d) f.push_back((*this)[f.size()]);
+            FPS h = f-g.log(d);
+            h[0] += 1;
+            g = (g*h).mod_deg(d);
+        }
+        return g.mod_deg(deg);
     }
 
     void shrink() {
@@ -35,14 +79,14 @@ struct FPS: vector<mint>{
     }
 
     FPS& operator+=(const FPS &r){
-       if(this->size() < r.size()) this->resize(r.size());
-       for(int i = 0; i < (int)r.size(); i++) (*this)[i] += r[i];
+        if(this->size() < r.size()) this->resize(r.size());
+        for(int i = 0; i < (int)r.size(); i++) (*this)[i] += r[i];
         return *this;
     }
 
     FPS& operator-=(const FPS &r){
-       if(this->size() < r.size()) this->resize(r.size());
-       for(int i = 0; i < (int)r.size(); i++) (*this)[i] -= r[i];
+        if(this->size() < r.size()) this->resize(r.size());
+        for(int i = 0; i < (int)r.size(); i++) (*this)[i] -= r[i];
         return *this;
     }
 
